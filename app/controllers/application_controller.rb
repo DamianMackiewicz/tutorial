d3daa917e001d1fc7bb076d8caac66ea38985144
DamianMackiewicz <damian.mckwcz@gmail.com>
2017-04-27 08:20:35 +0200

    Add files via upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-    def hello
-    render html: "hello, world!"
-    end
+  
+  def hello
+    render html: "Witaj swiecie!"
+  end
+  
 end
